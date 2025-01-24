8e86d85e261f77628a228e5441b3e2893f618526
jeromydean <jeromydean@gmail.com>
2025-01-24 11:23:26 -0500

    Add default Avalonia MVVM template
    Updated nuget references

---
+++ src/AvaloniaSandbox/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaSandbox.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
